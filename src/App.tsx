d2890e0fae6900109cba3a9cd95339a253c21b77
cruiz-designli <christian.ruiz@designli.co>
2024-11-11 16:13:02 -0500

    :sparkles: Add ProductList component to show all available products

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import ProductList from "./components/ProductList";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold">
-        Hello <span className="underline text-[#f87565]">Designli</span>
-      </h1>
-    </>
+    <main>
+      <h1>Products</h1>
+      <ProductList />
+    </main>
   );
 }
 
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { API_BASE_URL } from "../constants";
+import { Product } from "../types/products";
+
+const ProductList = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await fetch(`${API_BASE_URL}/products`);
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getProducts();
+  }, []);
+
+  return (
+    <section className="grid grid-cols-3 gap-4">
+      {products.map((product) => (
+        <article key={product.id} className="border rounded-lg overflow-hidden">
+          <img
+            src={product.images[0]}
+            alt={product.title}
+            className="object-cover w-full max-h-80"
+          />
+          <div className="p-4 space-y-2 text-left">
+            <div className="flex justify-between items-center">
+              <h2 className="text-xl font-bold">{product.title}</h2>
+              <div className="text-sm text-gray-500 bg-slate-50 px-2 py-1 rounded-full">
+                {product.category.name}
+              </div>
+            </div>
+            <p className="text-sm text-gray-500">{product.description}</p>
+            <p className="text-lg font-bold">${product.price}</p>
+          </div>
+        </article>
+      ))}
+    </section>
+  );
+};
+
+export default ProductList;
