21099916e1ed257182e1e320bb5bdecf76bb3a0c
cruiz-designli <christian.ruiz@designli.co>
2024-11-12 16:50:54 -0500

    :art: Add QUERY_PARAMS enum for better handling query params when filtering

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,4 +6,11 @@ const ROUTES = {
   PRODUCT_DETAIL: "/products/:id",
 };
 
-export { API_BASE_URL, ROUTES };
+enum API_QUERY_PARAMS {
+  CATEGORY = "categoryId",
+  PRICE_MIN = "price_min",
+  PRICE_MAX = "price_max",
+  TITLE = "title",
+}
+
+export { API_BASE_URL, ROUTES, API_QUERY_PARAMS };
