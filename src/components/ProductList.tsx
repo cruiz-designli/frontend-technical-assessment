f04f4ece75c2283c2cdb0af90c3244d1c996890e
cruiz-designli <christian.ruiz@designli.co>
2024-11-12 16:46:01 -0500

    :art: Add ProductCard component to both ProductList and Detail components

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,50 +1,17 @@
-import { useEffect } from "react";
-
-import { API_BASE_URL } from "../constants";
 import { Product } from "../types/products";
 import { Link } from "react-router-dom";
+import ProductCard from "./ProductCard";
 
 type ProductListProps = {
-  products: Product[];
-  handleProductsUpdate: (products: Product[]) => void;
+  data: Product[];
 };
 
-const ProductList = ({ products, handleProductsUpdate }: ProductListProps) => {
-  useEffect(() => {
-    const getProducts = async () => {
-      try {
-        const response = await fetch(`${API_BASE_URL}/products`);
-        const data = await response.json();
-        handleProductsUpdate(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    getProducts();
-  }, []);
-
+const ProductList = ({ data }: ProductListProps) => {
   return (
-    <section className="grid grid-cols-3 gap-4">
-      {products.map((product) => (
+    <section className="grid grid-cols-[repeat(auto-fill,_minmax(min(100%,_20em),_1fr))] gap-5 min-h-screen">
+      {data.map((product) => (
         <Link to={`/products/${product.id}`} key={product.id}>
-          <article className="border rounded-lg overflow-hidden">
-            <img
-              src={product.images[0]}
-              alt={product.title}
-              className="object-cover w-full max-h-80"
-            />
-            <div className="p-4 space-y-2 text-left">
-              <div className="flex justify-between items-center">
-                <h2 className="text-xl font-bold">{product.title}</h2>
-                <div className="text-sm text-gray-500 bg-slate-50 px-2 py-1 rounded-full">
-                  {product.category.name}
-                </div>
-              </div>
-              <p className="text-sm text-gray-500">{product.description}</p>
-              <p className="text-lg font-bold">${product.price}</p>
-            </div>
-          </article>
+          <ProductCard product={product} />
         </Link>
       ))}
     </section>
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 
 import { API_BASE_URL, ROUTES } from "../constants";
 import { Product } from "../types/products";
+import ProductCard from "../components/ProductCard";
 
 const Detail = () => {
   const { id } = useParams();
@@ -23,29 +24,17 @@ const Detail = () => {
     getProductById();
   }, [id]);
 
+  if (!product) {
+    return <div>Loading</div>;
+  }
+
   return (
     <div className="max-w-2xl mx-auto">
       <div className="flex items-start gap-6">
         <button className="text-blue-500" onClick={() => navigate(ROUTES.HOME)}>
           Back
         </button>
-        <article className="border rounded-lg overflow-hidden">
-          <img
-            src={product?.images[0]}
-            alt={product?.title}
-            className="object-cover w-full max-h-80"
-          />
-          <div className="p-4 space-y-2 text-left">
-            <div className="flex justify-between items-center">
-              <h2 className="text-xl font-bold">{product?.title}</h2>
-              <div className="text-sm text-gray-500 bg-slate-50 px-2 py-1 rounded-full">
-                {product?.category.name}
-              </div>
-            </div>
-            <p className="text-sm text-gray-500">{product?.description}</p>
-            <p className="text-lg font-bold">${product?.price}</p>
-          </div>
-        </article>
+        <ProductCard product={product} />
       </div>
     </div>
   );
