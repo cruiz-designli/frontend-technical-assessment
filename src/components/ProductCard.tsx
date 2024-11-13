a042ceab9fdcaabbdcb4e42334ded77104034c03
cruiz-designli <christian.ruiz@designli.co>
2024-11-12 19:37:09 -0500

    :sparkles: Update ProductCard component to include utility function for cleaning up urls and also add placeholder images for scenarios where images don't properly load

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,29 +1,32 @@
 import { Product } from "../types/products";
+import placeholderImage from "../assets/placeholder-image.jpg";
+import { cleanImageUrl } from "../utils/cleanImgUrls";
 
 type ProductCardProps = {
   product: Product;
 };
 
-const ProductCard = ({ product }: ProductCardProps) => {
-  return (
-    <article className="border rounded-lg overflow-hidden ">
-      <img
-        src={product.images[0]}
-        alt={product.title}
-        className="object-cover w-full max-h-80"
-      />
-      <div className="p-4 space-y-2 text-left">
-        <div className="flex justify-between items-center">
-          <h2 className="text-xl font-bold">{product.title}</h2>
-          <div className="text-sm text-gray-500 bg-slate-50 px-2 py-1 rounded-full">
-            {product.category.name}
-          </div>
+const ProductCard = ({ product }: ProductCardProps) => (
+  <article className="border rounded-lg overflow-hidden ">
+    <img
+      src={cleanImageUrl(product.images[0])}
+      alt={product.title}
+      className="object-cover w-full max-h-80"
+      onError={(e) => {
+        e.currentTarget.src = placeholderImage;
+      }}
+    />
+    <div className="p-4 space-y-2 text-left">
+      <div className="flex justify-between items-center">
+        <h2 className="text-xl font-bold">{product.title}</h2>
+        <div className="text-sm text-gray-500 bg-slate-50 px-2 py-1 rounded-full">
+          {product.category.name}
         </div>
-        <p className="text-sm text-gray-500">{product.description}</p>
-        <p className="text-lg font-bold">${product.price}</p>
       </div>
-    </article>
-  );
-};
+      <p className="text-sm text-gray-500">{product.description}</p>
+      <p className="text-lg font-bold">${product.price}</p>
+    </div>
+  </article>
+);
 
 export default ProductCard;
