4b6fd57f551eef4c4048eca75089c83941014bb9
cruiz-designli <christian.ruiz@designli.co>
2024-11-12 19:49:57 -0500

    :sparkles: Add placeholder image for empty results when searching or filtering

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,12 +1,26 @@
-import { Product } from "../types/products";
 import { Link } from "react-router-dom";
+
+import { Product } from "../types/products";
 import ProductCard from "./ProductCard";
+
+import noResultsImage from "../assets/no-results-found.webp";
 
 type ProductListProps = {
   data: Product[];
 };
 
 const ProductList = ({ data }: ProductListProps) => {
+  if (data.length === 0) {
+    return (
+      <img
+        src={noResultsImage}
+        alt="No results found"
+        width={500}
+        className="mx-auto rounded-md overflow-hidden"
+      />
+    );
+  }
+
   return (
     <section className="grid grid-cols-[repeat(auto-fill,_minmax(min(100%,_20em),_1fr))] gap-5 min-h-screen">
       {data.map((product) => (
